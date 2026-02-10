3ab5d7e63fb11b6049bb76af09ac7650bbd699dd
Ahsan alam <ashu.ansari84.indian@gmail.com>
2026-02-10 16:34:34 +0530

    Create gauss_seidel.py

---
+++ gauss_seidel.py
@@ -0,0 +1,20 @@
+import numpy as np
+
+A = np.array([[4,1,2],
+              [3,5,1],
+              [1,1,3]], dtype=float)
+
+B = np.array([4,7,3], dtype=float)
+
+x = np.zeros(len(B))
+
+for it in range(10):
+    for i in range(len(B)):
+        s = 0
+        for j in range(len(B)):
+            if j != i:
+                s += A[i][j] * x[j]
+        x[i] = (B[i] - s) / A[i][i]
+    print("Iteration", it+1, ":", x)
+
+print("Final solution:", x)
